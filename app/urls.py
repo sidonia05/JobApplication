a76ad2bf36d7fdab50e421dec6d517275bf6ed1a
sidonia05 <sidoniaraluca1994@gmail.com>
2023-08-22 08:17:14 +0300

    2

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from app.views import hello
+from app.views import hello, job_detail
 
 urlpatterns = [
-    path ('', hello)
+    path ('', hello),
+    path('job/1', job_detail),
 ]
--- app/views.py
+++ app/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello (request):
-    return HttpResponse ('Never give up. Today is hard, tomorrow will be worse, but the day after tomorrow will be sunshine.')
+    return HttpResponse ('Never give up. Today is hard, '
+                         'tomorrow will be worse, but the day after tomorrow will be sunshine.')
+
+def job_detail(request):
+    return HttpResponse ('Job detail page')
