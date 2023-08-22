4d023d9551cc321cf9962018334faaaa489325a9
sidonia05 <sidoniaraluca1994@gmail.com>
2023-08-22 08:02:13 +0300

    2

--- JobApplication/urls.py
+++ JobApplication/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from app.views import hello
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello),
+
 ]
